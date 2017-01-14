1eaa7f55edeb7975329b6af27959229ecafbfc11
jordanbrauer <jbrauer.inc@gmail.com>
2017-01-14 01:00:13 -0600

    Updated comments to JSDoc. Started Enemy.update method.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,51 @@
-/**
-Enemies our player must avoid
-@param int x starting x position on the canvas.
-@param int y starting y position on the canvas.
-@param int speed value to be multiplied by the time delta variable for smooth animation.
-*/
-var Enemy = function(x, y, speed)
-{
-    this.x = x; // Set the enemy x (horizontal) position.
-    this.y = y; // Set the enemy y (vertical) position.
-    this.speed = speed; // Set the enemy speed.
-    this.init = x; // Save the enemy's initial x position for resetting.
+/** Enemies our player must avoid. */
+var Enemy = function(x, y, speed) {
+  /**
+   * Set the enemys' initial x (horizontal) value.
+   * @param {int} x - The initial x value.
+   */
+  this.x = x;
 
-    this.sprite = 'images/enemy-bug.png'; // The sprite for our enemies. Using helper fn.
+  /**
+   * Set the enemys' initial y (vertical) value.
+   * @param {int} y - The initial y value.
+   */
+  this.y = y;
+
+  /**
+   * Set the enemy's speed. The value is multiplied by the time delta
+   * variable for smooth animation purposes.
+   * @param {int} speed - The enemies speed value.
+   */
+  this.speed = speed;
+
+  /**
+   * Save the enemy's initial x position for resetting.
+   * @prop {int} init - The init value. Equal to the initial x value.
+   */
+  this.init = x;
+
+  /**
+   * The sprite image used for our enemies.
+   * @prop {string} sprite - Image loaded via the `sprite` helper method.
+  */
+  this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/**
+* Update the enemy's position, required method for game
+* @param {int} dt a time delta between game ticks.
+*/
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
@@ -41,12 +62,12 @@ Enemy.prototype.render = function() {
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
