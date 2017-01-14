0fc244ae0510d19a1d088eea283d34bc492a7afd
jordanbrauer <jbrauer.inc@gmail.com>
2017-01-14 00:36:36 -0600

    Added enemy initial properties.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+/**
+Enemies our player must avoid
+@param int x starting x position on the canvas.
+@param int y starting y position on the canvas.
+@param int speed value to be multiplied by the time delta variable for smooth animation.
+*/
+var Enemy = function(x, y, speed)
+{
+    this.x = x; // Set the enemy x (horizontal) position.
+    this.y = y; // Set the enemy y (vertical) position.
+    this.speed = speed; // Set the enemy speed.
+    this.init = x; // Save the enemy's initial x position for resetting.
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = 'images/enemy-bug.png'; // The sprite for our enemies. Using helper fn.
 };
 
 // Update the enemy's position, required method for game
@@ -25,11 +31,11 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+// NOTE: use checkCollisions
 
 
 // This listens for key presses and sends the keys to your
