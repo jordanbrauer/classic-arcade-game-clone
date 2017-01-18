401c61a2ef9cc901f63b1681fbfd810e915c080c
jordanbrauer <jbrauer.inc@gmail.com>
2017-01-17 22:22:01 -0600

    Finallized JSDoc and organization.

--- js/app.js
+++ js/app.js
@@ -9,73 +9,23 @@
  * @param {int} Enemy.y - The initial y value.
  * @param {int} Enemy.speed - The enemies speed value.
  */
-var Enemy = function(x, y, speed) {
-  /**
-   * Set the enemys' initial x (horizontal) value.
-   * @prop {int} Enemy.x - The initial x value.
-   */
+
+var Enemy = function (x, y, speed)
+{
+  /** @property {int} Enemy.x - The initial x value. */
   this.x = x;
 
-  /**
-   * Set the enemys' initial y (vertical) value.
-   * @prop {int} Enemy.y - The initial y value.
-   */
+  /** @property {int} Enemy.y - The initial y value. */
   this.y = y;
 
-  /**
-   * Set the enemy's speed. The value is multiplied by the time delta
-   * variable for smooth animation purposes.
-   * @prop {int} Enemy.speed - The enemies speed value.
-   */
+  /** @property {int} Enemy.speed - The enemies speed value. */
   this.speed = speed;
 
-  /**
-   * Save the enemy's initial x position for resetting.
-   * @prop {int} Enemy.init - The init value. Equal to the initial x value.
-   */
+  /** @property {int} Enemy.init - The init value. Equal to the initial x value. */
   this.init = x;
 
-  /**
-   * The sprite image used for our enemies.
-   * @prop {string} Enemy.sprite - Image loaded via the `sprite` helper method.
-   */
+  /** @property {string} Enemy.sprite - Image loaded via the Resource helper method. */
   this.sprite = 'images/enemy-bug.png';
-};
-
-/**
- * @method Enemy.prototype.update
- * @desc Update the enemy's position, required method for game.
- * @param {int} dt - the time delta.
- */
-Enemy.prototype.update = function(dt) {
-  /**
-   * Multiply any movement by the time delta (dt) parameter which will ensure
-   * the game runs at the same speed for all computers.
-   */
-  this.x += this.speed * dt;
-
-  /**
-   * Check to see if enemy has moved past the canvas window.
-   * If they have, reset the enemy.
-   *
-   * TODO: Move edge detection logic into dedicated method and fire here..
-   */
-  if (this.x > 500) {
-    this.reset();
-  }
-
-  /**
-   * Check to see if the enemy collides with the Player object.
-   * If they do, reset the player and bug.
-   * If the Enemy colliding with the Player has an x value greather than or equal to
-   *
-   * TODO: Move Collision logic into dedicated method and fire here.
-   */
-  if ((this.x >= player.x - 50.5 && this.x <= player.x + 50.5)
-  && (this.y >= player.y - 50.5 && this.y <= player.y + 50.5)) {
-    this.reset();
-    player.reset();
-  }
 };
 
 /**
@@ -84,17 +34,97 @@ Enemy.prototype.update = function(dt) {
  * @desc Use the Resources helper to get the enemy sprite and place it
  * on the canvas at the Enemy default x and y coordinates.
  */
-Enemy.prototype.render = function() {
+
+Enemy.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+/**
+ * @method Enemy.prototype.move
+ * @summary Propel the Enemy vehicles/bugs forward on the canvas.
+ * @desc
+ */
+
+Enemy.prototype.move = function (direction, _speed) {
+  if (direction == 'right') {
+    this.x += _speed;
+  }
+  // Useful for switching up the direction of the bugs. First have to
+  // fix up the Enemy constructor for starting location.
+  // if (direction == 'left') {
+  //   this.x -= _speed;
+  // }
+};
+
+/**
+ * @method Enemy.prototype.checkEdges
+ * @summary Check if the Enemy has passed the edges of the canvas.
+ * @param {obj} obj - The object to check against.
+ * @desc If the Enemy passing the canvas edges has an x value greater
+ * than the width of the canvas, reset the Enemy. This function could be more
+ * robust if they bugs were omnidirectional.
+ */
+
+Enemy.prototype.checkEdges = function (obj) {
+  if (this.x > obj) {
+    this.reset();
+  }
+};
+
+/**
+ * @method Enemy.prototype.checkCollision
+ * @summary Check for collision with the Player. Can be used for other things too.
+ * @param {obj} obj - Object to check against.
+ * @desc If both this Enemy colliding with the Player has an x value greather than
+ * or eqaul to the Players' x value (subtracting half of the Players' width for
+ * "edge detection" on the left) AND has an x value less than or equal to the
+ * Players' x value (adding half of the Players' width for "edge detection" on the
+ * right), are true AND the exact same for the y coordinates are both true, than
+ * reset the Player and this Enemy. Bet you thought I was going to repeat the
+ * whole thing for y coordinates as well. ;-)
+ */
+
+Enemy.prototype.checkCollision = function (obj) {
+  if ((this.x >= obj.x - 50.5 && this.x <= obj.x + 50.5)
+  && (this.y >= obj.y - 50.5 && this.y <= obj.y + 50.5)) {
+    /** Reset this enemy */
+    this.reset();
+
+    /** If the obj was the Player, reset them. */
+    if (obj == player) {
+      player.reset();
+    }
+  }
 };
 
 /**
  * @method Enemy.prototype.reset
  * @desc Reset the enemy upon collision or travelling off the canvas.
  */
-Enemy.prototype.reset = function() {
+
+Enemy.prototype.reset = function () {
   /** Set the enemy x value to the initial value saved in the init property. */
   this.x = this.init;
+};
+
+/**
+ * @method Enemy.prototype.update
+ * @summary Update the enemys on screen.
+ * @param {int} dt - the time delta. Used for movement events.
+ * @desc The update function for the enemy will fire of all enemy events. Put any
+ * events in here for the enemies. Create a method and necessary properties on the
+ * Enemy and call it in here.
+ */
+
+Enemy.prototype.update = function (dt) {
+  /** Multiply any movement by the time delta (dt) parameter which will ensure the game runs at the same speed for all computers. */
+  this.move('right', this.speed * dt);
+
+  /** Check for collisions with the player. */
+  this.checkCollision(player);
+
+  /** Check to see if enemy has passed the canvas edge. */
+  this.checkEdges(ctx.canvas.width);
 };
 
 //-----------------------------------------------
@@ -107,71 +137,55 @@ Enemy.prototype.reset = function() {
  * @param {int} Player.x - The initial x value of the Player.
  * @param {int} Player.y - The initial y value of the Player.
  */
-var Player = function(x, y) {
-  /**
-   * Set intial x (horizontal) coordinates on the canvas.
-   * @prop {int} Player.x - The initial x value of the Player.
-   */
+
+var Player = function (x, y)
+{
+  /** @property {int} Player.x - The initial x value of the Player. */
    this.x = x;
 
-  /**
-   * Set initial y (vertical) coordinates on the canvas.
-   * @prop {int} Player.y - The initial y value of the Player.
-   */
+  /** @property {int} Player.y - The initial y value of the Player. */
   this.y = y;
 
-  /**
-   * Store initial x and y coordinates in an object for reseting the player
-   * upon death or winning.
-   * @prop {obj} Player.init - Store the initial starting location for the player.
-   */
+  /** @property {obj} Player.init - Store the initial starting location for the player. */
   this.init = {
     "x": x,
     "y": y,
   };
 
-  /**
-   * Give the player a sprite for visualization.
-   * @prop {string} Player.sprite - Image to be loaded via the Resource helper.
-   */
+  /** @property {string} Player.sprite - Image to be loaded via the Resource helper. */
   this.sprite = 'images/char-boy.png';
-};
 
-/**
- * @method Player.prototype.update
- * @desc Player update function.
- * @param {int} dt - The time delta.
- */
-Player.prototype.update = function(dt) {
-  /** multiply the player speed by the time delta variable for smooth animation. */
-  this.x += this.speed * dt;
+  /** @property {obj} Player.controls - Object array of player controls. */
+  this.controls = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+  };
 };
-
-/**
- * BUG: The player does not seem to render unless Player.prototype.update is
- * defined twice.Not sure if this is a bug or what I am doing wrong...
- */
-Player.prototype.update = function() {};
 
 /**
  * @method Player.prototype.render
- * @desc Player render function.
+ * @summary Player render function.
+ * @description Use the Resources helper to get the Player sprite and place it on
+ * the canvas at the Player default x and y coordinates.
  */
-Player.prototype.render = function() {
-  /**
-   * Use the Resources helper to get the Player sprite and place it on the canvas
-   * at the Player default x and y coordinates.
-   */
+
+Player.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 /**
  * @method Player.handleInput
- * @desc Player handleInput fucntion.
- * @fires Player.move(direction)
+ * @summary Player handleInput function.
+ * @fires Player.move
  * @fires Player.checkForFinishLine
+ * @description Handle the input events for player controls and make any necessary
+ * checks that should be made before or after a player action is made, e.g.,
+ * landing on the finish line after moving squares.
  */
-Player.prototype.handleInput = function() {
+
+Player.prototype.handleInput = function (e) {
   /** Use a switch statement to handle input from the 'keyup' document event listener. */
   switch (event.keyCode) {
     case 37:
@@ -186,6 +200,9 @@ Player.prototype.handleInput = function() {
     case 40:
       this.move('down');
       break;
+
+    /** Prevent any double actions (e.g., scrolling page with arrow keys) through the preventDefault(); event method. */
+    event.preventDefault();
   }
 
   /** Fire the checkForFinishLine() method to determine if we've landed on a finish line. */
@@ -194,52 +211,42 @@ Player.prototype.handleInput = function() {
 
 /**
  * @method Player.prototype.move
+ * @summary Move the player in one of four directions.
  * @param {string} direction - The direction in which to move the player.
+ * @description The logic for player movement. We start by getting the width and
+ * height of a square. We then check which direction has been requested and act
+ * accordingly. Left/Right, subtract/add width of tile from players x value.
+ * Up/Down, subtract/add height of tile divided by 2 from players y value.
  */
-Player.prototype.move = function(direction) {
-  /**
-   * Get a tile resource to measure as reference for player movement.
-   * @var {object} tile
-   */
+
+Player.prototype.move = function (direction) {
+  /** @var {object} tile - Used as reference.*/
   var tile = Resources.get('images/grass-block.png');
 
-  /**
-   * The width of a tile. Used to move the Player on screen.
-   * @var {int} tileWidth
-   */
+  /** @var {int} tileWidth - Width of reference tile */
   var tileWidth = tile.width;
 
-  /**
-   * The height of a tile halved (slightly tweaked to improve the placement of the Player).
-   * @var {int} tileHalfHeight
-   */
+  /** @var {int} tileHalfHeight - Height of refernce tile (slightlty tweaked). */
   var tileHalfHeight = tile.height / 2.09;
 
-  /** If direction from Player.prototype.handleInput is left, move left. */
   if (direction == 'left') {
     if (this.x >= 0) {
       return this.x -= tileWidth;
     }
     // console.warn(`You can\'t move ${direction} any farther.`);
   }
-
-  /** If direction from Player.prototype.handleInput is up, move up. */
   if (direction == 'up') {
     if (this.y > 0) {
       return this.y -= tileHalfHeight;
     }
     // console.warn(`You can\'t move ${direction} any farther.`);
   }
-
-  /** If direction from Player.prototype.handleInput is right, move right. */
   if (direction == 'right') {
     if (this.x < 400) {
       return this.x += tileWidth;
     }
     // console.warn(`You can\'t move ${direction} any farther.`);
   }
-
-  /** If direction from Player.prototype.handleInput is down, move down. */
   if (direction == 'down') {
     if (this.y < 400) {
       return this.y += tileHalfHeight;
@@ -250,8 +257,12 @@ Player.prototype.move = function(direction) {
 
 /**
  * @method Player.prototype.checkForFinishLine
- * @desc Check if the player has gotten to the finish line!
+ * @summary Check if the player has gotten to the finish line!
+ * @fires Player.reset
+ * @description Check the player's Y coordinates to see if they have reached the
+ * finish line (in our case, the water).
  */
+
 Player.prototype.checkForFinishLine = function () {
   if (this.y < 0) {
     this.reset();
@@ -260,12 +271,27 @@ Player.prototype.checkForFinishLine = function () {
 
 /**
  * @method Player.prototype.reset
- * @desc Reset the Player upon collision.
+ * @summary Reset the Player to initial starting coordinates.
+ * @description Method used for reseting the player. This methods uses the values
+ * that are stored within the Playr.init object property upon player instantiation.
  */
+
 Player.prototype.reset = function () {
   this.x = this.init.x;
   this.y = this.init.y;
 };
+
+/**
+ * @method Player.prototype.update
+ * @summary Update player object.
+ * @param {int} dt - The time delta.
+ * @description Update player object according to the time delta parameter.
+ */
+
+Player.prototype.update = function (dt) {
+  // ... unused.
+};
+
 
 //-----------------------------------------------
 // Object Instantiation
@@ -274,31 +300,43 @@ Player.prototype.reset = function () {
 /** Instantiate the player with a set of x and y coordinates. */
 var player = new Player(200, 400);
 
-/**
- * Populate the allEnemies array with new Enemy objects. Each row in the array
- * corresponds to the row on the canvas.
- */
+/** Populate the allEnemies array with new Enemy objects. Each row in the array corresponds to the row on the canvas. */
 var allEnemies = [
-  new Enemy(-200, 60, 450),
-  new Enemy(-200, 145, 210),
-  new Enemy(-200, 225, 350),
+  new Enemy(-200, 60, setRandomSpeed(225, 550)),
+  new Enemy(-200, 145, setRandomSpeed(225, 550)),
+  new Enemy(-200, 225, setRandomSpeed(225, 550)),
 ];
 
 //-----------------------------------------------
 // Event Listeners
 //-----------------------------------------------
 
-/**
- * This listens for key presses and sends the keys to your Player.handleInput()
- * method. You don't need to modify this.
- */
+/** This listens for key presses and sends the keys to your Player.handleInput() method. You don't need to modify this. */
 document.addEventListener('keyup', function(e) {
-  var allowedKeys = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-  };
-
-  player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(player.controls[e.keyCode]);
 });
+
+//-----------------------------------------------
+// Helper Functions
+//-----------------------------------------------
+
+/**
+ * @function setRandomSpeed
+ * @summary Set the enemy speed randomly.
+ * @argument {int} min - Minimum speed value.
+ * @argument {int} max - Maximum speed value.
+ * @returns {int}
+ * @description Set the enemy speed to a random integer between two values.
+ * Useful for varrying the Enemy "behaviour" each time the game is loaded.
+ */
+
+function setRandomSpeed (min, max) {
+  /** @var {int} min - Minimum value rounded up. */
+  var min = Math.ceil(min);
+
+  /** @var {int} max - Maximum value rounded down. */
+  var max = Math.floor(max);
+
+  /** Generate a random random number, multiply it by the difference of max and min, round the result down and add min. */
+  return Math.floor(Math.random() * (max - min)) + min;
+}
